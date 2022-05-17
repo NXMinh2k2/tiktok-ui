50111ba52c8a8232bb4fea9f4eb178e477b8f2b4
NXMinh2k2 <minhxuannguyen0809@gmail.com>
2022-05-17 16:55:28 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,30 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo}></img>
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx('clear')}>
+            <i className="fa-solid fa-xmark"></i>
+          </button>
+          <button className={cx('loading')}>
+            <i className="fa-solid fa-spinner"></i>
+          </button>
+          <button className={cx('search-btn')}>
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+        <div className={cx('action')}></div>
+      </div>
     </header>
   );
 }
